78bad8fb284fdab2d4175053c66aaa07ce1e7cec
Mayuresh Sarpotdar <mayuresh159@gmail.com>
2021-07-05 14:19:13 +0530

    Organized gnuradio files

---
+++ gnuradio/IIST_IS1_UHF_SDR_tx_only.py
@@ -0,0 +1,328 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: IIST INSPIRE Ground Station
+# Author: IIST
+# Description: Copyright 2021 IIST, Thiruvananthapuram
+# GNU Radio version: 3.7.14.0
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt4 import Qt
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import qtgui
+from gnuradio import uhd
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from gnuradio.qtgui import Range, RangeWidget
+from optparse import OptionParser
+import kiss
+import sip
+import sys
+import time
+from gnuradio import qtgui
+
+
+class IIST_IS1_UHF_SDR_tx_only(gr.top_block, Qt.QWidget):
+
+    def __init__(self, tx_freq=437.5e6, tx_offset=250e3):
+        gr.top_block.__init__(self, "IIST INSPIRE Ground Station")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("IIST INSPIRE Ground Station")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "IIST_IS1_UHF_SDR_tx_only")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.tx_freq = tx_freq
+        self.tx_offset = tx_offset
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 240000
+        self.baud = baud = 9600
+        self.tx_gain = tx_gain = 10
+        self.tx_correct = tx_correct = 0
+        self.samps_per_symb = samps_per_symb = samp_rate/baud
+        self.interp = interp = 25
+        self.decim = decim = 24
+        self.bb_gain = bb_gain = .95
+        self.alpha = alpha = .5
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self._tx_gain_range = Range(0, 86, 1, 10, 200)
+        self._tx_gain_win = RangeWidget(self._tx_gain_range, self.set_tx_gain, 'TX Gain', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._tx_gain_win, 7, 0, 1, 4)
+        for r in range(7, 8):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 4):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self._tx_correct_range = Range(-10000, 10000, 1, 0, 200)
+        self._tx_correct_win = RangeWidget(self._tx_correct_range, self.set_tx_correct, "tx_correct", "counter_slider", float)
+        self.top_grid_layout.addWidget(self._tx_correct_win, 8, 0, 1, 4)
+        for r in range(8, 9):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 4):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self._bb_gain_range = Range(0, 1, .01, .95, 200)
+        self._bb_gain_win = RangeWidget(self._bb_gain_range, self.set_bb_gain, 'bb_gain', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._bb_gain_win, 6, 0, 1, 4)
+        for r in range(6, 7):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 4):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.uhd_usrp_sink_0_0 = uhd.usrp_sink(
+        	",".join(("", "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_sink_0_0.set_samp_rate(250e3)
+        self.uhd_usrp_sink_0_0.set_time_now(uhd.time_spec(time.time()), uhd.ALL_MBOARDS)
+        self.uhd_usrp_sink_0_0.set_center_freq(uhd.tune_request(tx_freq+tx_correct, tx_offset), 0)
+        self.uhd_usrp_sink_0_0.set_gain(tx_gain, 0)
+        self.uhd_usrp_sink_0_0.set_antenna('TX/RX', 0)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=interp,
+                decimation=decim,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.qtgui_freq_sink_x_1 = qtgui.freq_sink_c(
+        	1024, #size
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	0, #fc
+        	samp_rate/decim * interp, #bw
+        	"TX Spectrum", #name
+        	1 #number of inputs
+        )
+        self.qtgui_freq_sink_x_1.set_update_time(0.010)
+        self.qtgui_freq_sink_x_1.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_1.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_1.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_1.enable_autoscale(False)
+        self.qtgui_freq_sink_x_1.enable_grid(True)
+        self.qtgui_freq_sink_x_1.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_1.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_1.enable_control_panel(False)
+
+        if not False:
+          self.qtgui_freq_sink_x_1.disable_legend()
+
+        if "complex" == "float" or "complex" == "msg_float":
+          self.qtgui_freq_sink_x_1.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_1.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_1.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_1.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_1.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_1.set_line_alpha(i, alphas[i])
+
+        self._qtgui_freq_sink_x_1_win = sip.wrapinstance(self.qtgui_freq_sink_x_1.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_1_win, 2, 0, 4, 4)
+        for r in range(2, 6):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 4):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.kiss_nrzi_encode_0 = kiss.nrzi_encode()
+        self.kiss_hdlc_framer_0 = kiss.hdlc_framer(preamble_bytes=64, postamble_bytes=64)
+        self.digital_scrambler_bb_0 = digital.scrambler_bb(0x21, 0x0, 16)
+        self.digital_gfsk_mod_0 = digital.gfsk_mod(
+        	samples_per_symbol=int(samps_per_symb),
+        	sensitivity=0.1,
+        	bt=alpha,
+        	verbose=False,
+        	log=False,
+        )
+        self.blocks_tag_gate_0 = blocks.tag_gate(gr.sizeof_gr_complex * 1, False)
+        self.blocks_tag_gate_0.set_single_key("packet_len")
+        self.blocks_socket_pdu_0_2 = blocks.socket_pdu("TCP_CLIENT", '0.0.0.0', '50000', 1024, False)
+        self.blocks_pdu_to_tagged_stream_0_0 = blocks.pdu_to_tagged_stream(blocks.byte_t, 'packet_len')
+        self.blocks_pack_k_bits_bb_0 = blocks.pack_k_bits_bb(8)
+        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_vcc((bb_gain, ))
+        self.blocks_message_debug_0 = blocks.message_debug()
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.blocks_socket_pdu_0_2, 'pdus'), (self.blocks_message_debug_0, 'print_pdu'))
+        self.msg_connect((self.blocks_socket_pdu_0_2, 'pdus'), (self.kiss_hdlc_framer_0, 'in'))
+        self.msg_connect((self.kiss_hdlc_framer_0, 'out'), (self.blocks_pdu_to_tagged_stream_0_0, 'pdus'))
+        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.digital_gfsk_mod_0, 0))
+        self.connect((self.blocks_pdu_to_tagged_stream_0_0, 0), (self.kiss_nrzi_encode_0, 0))
+        self.connect((self.blocks_tag_gate_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
+        self.connect((self.digital_gfsk_mod_0, 0), (self.blocks_tag_gate_0, 0))
+        self.connect((self.digital_scrambler_bb_0, 0), (self.blocks_pack_k_bits_bb_0, 0))
+        self.connect((self.kiss_nrzi_encode_0, 0), (self.digital_scrambler_bb_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.qtgui_freq_sink_x_1, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.uhd_usrp_sink_0_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "IIST_IS1_UHF_SDR_tx_only")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_tx_freq(self):
+        return self.tx_freq
+
+    def set_tx_freq(self, tx_freq):
+        self.tx_freq = tx_freq
+        self.uhd_usrp_sink_0_0.set_center_freq(uhd.tune_request(self.tx_freq+self.tx_correct, self.tx_offset), 0)
+
+    def get_tx_offset(self):
+        return self.tx_offset
+
+    def set_tx_offset(self, tx_offset):
+        self.tx_offset = tx_offset
+        self.uhd_usrp_sink_0_0.set_center_freq(uhd.tune_request(self.tx_freq+self.tx_correct, self.tx_offset), 0)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.set_samps_per_symb(self.samp_rate/self.baud)
+        self.qtgui_freq_sink_x_1.set_frequency_range(0, self.samp_rate/self.decim * self.interp)
+
+    def get_baud(self):
+        return self.baud
+
+    def set_baud(self, baud):
+        self.baud = baud
+        self.set_samps_per_symb(self.samp_rate/self.baud)
+
+    def get_tx_gain(self):
+        return self.tx_gain
+
+    def set_tx_gain(self, tx_gain):
+        self.tx_gain = tx_gain
+        self.uhd_usrp_sink_0_0.set_gain(self.tx_gain, 0)
+
+
+    def get_tx_correct(self):
+        return self.tx_correct
+
+    def set_tx_correct(self, tx_correct):
+        self.tx_correct = tx_correct
+        self.uhd_usrp_sink_0_0.set_center_freq(uhd.tune_request(self.tx_freq+self.tx_correct, self.tx_offset), 0)
+
+    def get_samps_per_symb(self):
+        return self.samps_per_symb
+
+    def set_samps_per_symb(self, samps_per_symb):
+        self.samps_per_symb = samps_per_symb
+
+    def get_interp(self):
+        return self.interp
+
+    def set_interp(self, interp):
+        self.interp = interp
+        self.qtgui_freq_sink_x_1.set_frequency_range(0, self.samp_rate/self.decim * self.interp)
+
+    def get_decim(self):
+        return self.decim
+
+    def set_decim(self, decim):
+        self.decim = decim
+        self.qtgui_freq_sink_x_1.set_frequency_range(0, self.samp_rate/self.decim * self.interp)
+
+    def get_bb_gain(self):
+        return self.bb_gain
+
+    def set_bb_gain(self, bb_gain):
+        self.bb_gain = bb_gain
+        self.blocks_multiply_const_vxx_0_0.set_k((self.bb_gain, ))
+
+    def get_alpha(self):
+        return self.alpha
+
+    def set_alpha(self, alpha):
+        self.alpha = alpha
+
+
+def argument_parser():
+    description = 'Copyright 2021 IIST, Thiruvananthapuram'
+    parser = OptionParser(usage="%prog: [options]", option_class=eng_option, description=description)
+    parser.add_option(
+        "", "--tx-freq", dest="tx_freq", type="eng_float", default=eng_notation.num_to_str(437.5e6),
+        help="Set tx_freq [default=%default]")
+    parser.add_option(
+        "", "--tx-offset", dest="tx_offset", type="eng_float", default=eng_notation.num_to_str(250e3),
+        help="Set tx_offset [default=%default]")
+    return parser
+
+
+def main(top_block_cls=IIST_IS1_UHF_SDR_tx_only, options=None):
+    if options is None:
+        options, _ = argument_parser().parse_args()
+
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls(tx_freq=options.tx_freq, tx_offset=options.tx_offset)
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
